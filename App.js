4ea009fc113908df22f7b195fe64b076baa526ef
Pablo Julian Alonso Olivera <pepoalonso1@gmail.com>
2022-11-17 23:48:08 -0300

    creando repositorio

--- App.js
+++ App.js
@@ -1,11 +1,131 @@
-import { StatusBar } from 'expo-status-bar';
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithRedirect,
+  getRedirectResult,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+import {
+  getFirestore,
+  addDoc,
+  collection,
+  getDocs,
+  doc,
+  updateDoc,
+  deleteDoc,
+} from 'firebase/firestore';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Button } from './components/Button';
+import app from './firebase';
+
 export default function App() {
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth(app);
+  const db = getFirestore(app);
+
+  const onAuth = () => {
+    signInWithRedirect(auth, provider);
+  };
+
+  const createAuthWithEmailAndPassword = () => {
+    createUserWithEmailAndPassword(auth, 'something@gmail.com', 'xqsi1234')
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const loginAuthWithEmailAndPassword = () => {
+    signInWithEmailAndPassword(auth, 'something@gmail.com', 'xqsi1234')
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const getSignInData = () => {
+    getRedirectResult(auth)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        const user = result?.user;
+        console.log(token, user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const createDataCollection = async () => {
+    const infoUser = {
+      product: 'Reloj',
+      category: 'Tiempo',
+      price: [
+        {
+          money: 'USD',
+          quantity: 100,
+        },
+      ],
+    };
+
+    try {
+      await addDoc(collection(db, 'cart'), infoUser);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getColletionData = async () => {
+    const { docs } = await getDocs(collection(db, 'users'));
+
+    const userMapped = docs.map((user) => user.data());
+    console.log(userMapped);
+  };
+
+  const updateColletionData = async () => {
+    const userSelected = doc(db, 'cart', 'Opz4MRzkWYvLjxrIUL3g');
+
+    try {
+      await updateDoc(userSelected, {
+        product: 'Mateo Max',
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const deleteColletionData = async () => {
+    const userSelected = doc(db, 'cart', 'Opz4MRzkWYvLjxrIUL3g');
+
+    try {
+      await deleteDoc(userSelected);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getSignInData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Sign-in with Google" onPress={onAuth} />
+      <Button title="Create user" onPress={createAuthWithEmailAndPassword} />
+      <Button title="Login user" onPress={loginAuthWithEmailAndPassword} />
+      <Button title="Create Data Collection" onPress={createDataCollection} />
+      <Button title="Get User Data" onPress={getColletionData} />
+      <Button title="Update Product Data" onPress={updateColletionData} />
+      <Button title="Erase Product Data" onPress={deleteColletionData} />
     </View>
   );
 }
@@ -13,8 +133,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00563f',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: 'orange',
+    padding: 10,
   },
 });
---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBYtufVme_2l_1fn1kA9N5QqZNIYUMt4Jk",
+  authDomain: "react-native-firebase-26766.firebaseapp.com",
+  projectId: "react-native-firebase-26766",
+  storageBucket: "react-native-firebase-26766.appspot.com",
+  messagingSenderId: "1024340901475",
+  appId: "1:1024340901475:web:ae8855988b64a45a410256"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
